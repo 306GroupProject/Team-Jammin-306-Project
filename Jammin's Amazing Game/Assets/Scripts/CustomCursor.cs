a3e528b25ea588a386f80754d35aec61b0f4d520
Mark <mjc862@mail.usask.ca>
2018-09-18 18:54:31 -0600

    Created a custom Cursor, with particle effects!

---
+++ Jammin's Amazing Game/Assets/Scripts/CustomCursor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCursor : MonoBehaviour {
+
+    public Texture2D cursor;
+    public GameObject particleSprite;
+    public float followSpeed = 1.0f;
+   
+    
+	// Use this for initialization
+	void Start () {
+        particleSprite.transform.position = transform.position;
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 mousePosition = Input.mousePosition;
+        
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        transform.position = Vector2.Lerp(transform.position, mousePosition, followSpeed);
+    }
+}
--- Jammin's Amazing Game/Assets/Scripts/FollowMousePosition.cs
+++ Jammin's Amazing Game/Assets/Scripts/FollowMousePosition.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 // A simple script which makes a gameObject follow the player's mouse cursor
 public class FollowMousePosition : MonoBehaviour {
-
+  
     public float followSpeed = 0.1f; // admittedly it may be better to just directly set the position to that of the mouse, but the smoothing offered by Lerp makes it look a little nicer
+    public GameObject cursor;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void Start() {
+        cursor = GetComponent<GameObject>();
+       
+    }
+
+
+    // Update is called once per frame
+    void Update () {
         Vector2 mousePosition = Input.mousePosition;
 
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
         transform.position = Vector2.Lerp(transform.position, mousePosition, followSpeed);
-	}
+    }
 }
