74f02b88b9e4a55aa0db4402d05f539b47281d34
Nick <43053345+nkind@users.noreply.github.com>
2018-09-24 20:35:52 -0600

    Added a simple basic attack on left-click

---
+++ Jammin's Amazing Game/Assets/Scripts/ActiveShotTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveShotTime : MonoBehaviour {
+
+    //destroy an attack after set time
+    public float shotTime = 0.02f;
+
+	void Start () {
+        Destroy(gameObject, shotTime);
+	}
+
+}
