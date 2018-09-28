4380c701745b476bc6bf411e02dcba662bc60c57
Nick <43053345+nkind@users.noreply.github.com>
2018-09-28 12:59:45 -0600

    Added attack with updated version of the game

--- Jammin's Amazing Game/Assets/Scripts/CharacterMovement.cs
+++ Jammin's Amazing Game/Assets/Scripts/CharacterMovement.cs
@@ -5,11 +5,15 @@ using UnityEngine.Networking;
 
 
 
-public class CharacterMovement : MonoBehaviour {
+public class CharacterMovement : NetworkBehaviour {
 
     [SerializeField] private float speed = 20.0f; // use to calculate how much force to be added. If player slips a lot, modify linear drag in RB toolbar
     private Rigidbody2D rb; // Get the RigidBody Component of our player to access mass and force
     private Animator anim;
+
+    public GameObject attackPrefab;
+    public float attackRate = 0.5f;
+    private float canAttack = 0.0f;
 
 
 	void Start () {
@@ -51,6 +55,28 @@ public class CharacterMovement : MonoBehaviour {
 
 
         rb.AddForce(movement * speed);
+
+        if (Input.GetMouseButtonDown(0) && Time.time > canAttack)
+        {
+            CmdAttack();
+            canAttack = Time.time + attackRate;
+        }
         
 	}
+
+    [Command]
+    void CmdAttack()
+    {
+        Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 direction = target - myPos;
+        direction = direction.normalized;
+
+        GameObject attack = (GameObject)Instantiate(attackPrefab, myPos, transform.rotation);
+        attack.GetComponent<Rigidbody2D>().velocity = direction * 20.0f;
+
+        NetworkServer.Spawn(attack);
+
+        Destroy(attack, 1.0f);
+    }
 }
