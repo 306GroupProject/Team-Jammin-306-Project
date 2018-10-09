2efd8af7bd418c5dda0715532e87323ca1777eac
Mark <mjc862@mail.usask.ca>
2018-10-08 20:25:08 -0600

    Added Ability Scripts and Custom Network Script

---
+++ Jammin's Amazing Game/Assets/Scripts/Abilities/Abilities.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public abstract class Abilities : ScriptableObject {
+
+    public string skillName;
+    public string description;
+    public float manaCost;
+    public float cooldown;
+    public MonoScript abilityScript;
+    public GameObject projectile;
+
+    public abstract void Cast();
+
+}
---
+++ Jammin's Amazing Game/Assets/Scripts/Abilities/Finn/HolyWater.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolyWater : Abilities {
+    
+    public override void Cast() {
+        Debug.Log("Casted Holy Water!");
+    }
+}
