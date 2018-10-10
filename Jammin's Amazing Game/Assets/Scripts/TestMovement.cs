511d57746011846ed9c71b54ea101e1f39ad0565
Mark <mjc862@mail.usask.ca>
2018-10-09 18:26:29 -0600

    Test branch to see if multiple prefabs work

---
+++ Jammin's Amazing Game/Assets/Scripts/TestMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class TestMovement : NetworkBehaviour {
+
+    private Rigidbody2D rb;
+    public float speed = 50.0f;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (!this.GetComponent<NetworkIdentity>().isLocalPlayer) {
+            return;
+        }
+
+        Vector2 vertical = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.AddForce(vertical * speed);
+    }
+}
