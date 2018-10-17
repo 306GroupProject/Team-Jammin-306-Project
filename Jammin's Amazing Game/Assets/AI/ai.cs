a89bcfe5bf3e891ff23f71b0b50459561b6d7787
Mark <mjc862@mail.usask.ca>
2018-10-16 19:20:54 -0600

    Bug Fixes On player stuff

--- Jammin's Amazing Game/Assets/AI/ai.cs
+++ Jammin's Amazing Game/Assets/AI/ai.cs
@@ -101,7 +101,7 @@ public class ai : NetworkBehaviour {
 		
 		if (coll.gameObject.tag == "basicAttack") {
 			// we can change were damage goes later. I just put it in there for now.
-			this.GetComponent<aiHealth>().Damage(playerPosition.GetComponent<CharacterMovement>().plyerDmg); 
+			this.GetComponent<aiHealth>().Damage(playerPosition.GetComponent<CharacterController>().plyerDmg); 
 			
 		}
 		
@@ -110,7 +110,7 @@ public class ai : NetworkBehaviour {
 
 //------------[[Decisions: Boolean]]------------------//
 	public bool enemySpotted(){
-		playerPosition = GameObject.FindWithTag("Player");
+		playerPosition = GameObject.FindWithTag("Player1");
 		// is enemySpotted this is our first decision!
 		if (Vector2.Distance (this.transform.position, playerPosition.transform.position) < 10f) {
 
