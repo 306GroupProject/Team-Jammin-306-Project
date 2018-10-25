f99b5441cc52b8fb8088bc3f015a63d5b1d613b0
Joel-Madsen <43053989+Joel-Madsen@users.noreply.github.com>
2018-10-25 16:34:41 -0600

    Created fire particles effects & updated Teleport
    Made 3 particle effects to be used for an "on fire" status. Added to the teleport so that user cannot teleport through gameObjects with the tag "Wall". However this created a bug with the cooldown UI...

--- Jammin's Amazing Game/Assets/Scripts/Abilities/SpellCooldowns.cs
+++ Jammin's Amazing Game/Assets/Scripts/Abilities/SpellCooldowns.cs
@@ -7,23 +7,33 @@ public class SpellCooldowns : MonoBehaviour {
     public Image cooldownImage;
     public float cooldown = 3; // set to 3 seconds for the teleport at the moment
     bool cooldownActive;
+    bool blocked = false;
 
     // Update is called once per frame
     void Update () {
-		if(Input.GetMouseButtonDown(1)) // if the player right clicks to teleport, activate the cooldown UI
-        {
-            cooldownActive = true;
-        }
 
-        if(cooldownActive)
+        if (!blocked)
         {
-            cooldownImage.fillAmount += 1 / cooldown * Time.deltaTime;
-
-            if(cooldownImage.fillAmount >= 1) // after the radial animation plays, set everything back to its default state
+            if (Input.GetMouseButtonDown(1)) // if the player right clicks to teleport, activate the cooldown UI
             {
-                cooldownImage.fillAmount = 0;
-                cooldownActive = false;
+                cooldownActive = true;
+            }
+
+            if (cooldownActive)
+            {
+                cooldownImage.fillAmount += 1 / cooldown * Time.deltaTime;
+
+                if (cooldownImage.fillAmount >= 1) // after the radial animation plays, set everything back to its default state
+                {
+                    cooldownImage.fillAmount = 0;
+                    cooldownActive = false;
+                }
             }
         }
 	}
+
+    public void TeleportBlocked(bool isBlocked)
+    {
+        blocked = isBlocked;
+    }
 }
