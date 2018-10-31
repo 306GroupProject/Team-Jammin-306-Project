8571f962df904cb67922899c9ad0e9530615ad33
Mark <mjc862@mail.usask.ca>
2018-10-30 21:58:31 -0600

    Bug Fixes to Zap's Movement, Modification to rockthrow radius and Modification to teleport script!

---
+++ Jammin's Amazing Game/Assets/Scripts/Abilities/Teleport.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Teleport : NetworkBehaviour {
+
+    [SyncVar(hook = "MovePlayer")] private Transform playerTransform; // the player's position
+    public float teleportCooldown = 3f;
+    private float timeSinceTeleport = 0f;
+    private Vector2 point; // the point where the mouse is clicked for a teleport
+    public GameObject teleportParticles;
+    public LayerMask wallMask; // a masking layer for walls that the player CANNOT teleport through
+    public float teleportDistance = 10.0f;
+
+    private void Awake() {
+        playerTransform = this.transform;
+    }
+
+    private void Update() {
+        if (isLocalPlayer) {
+            // if the player Right Clicks, teleport them to where they clicked.
+            if (Input.GetMouseButtonDown(1)) {
+                Vector2 direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, teleportDistance, wallMask);
+                if (!hit) // if there isn't a wall (gameObject with the tag "Wall") in the way, teleport if the cooldown is over
+                {
+                    if (timeSinceTeleport <= Time.time) {
+                        //cooldownScript.TeleportBlocked(false);
+                        CmdTeleport();
+                    }
+                } else {
+                    //cooldownScript.TeleportBlocked(true);
+                }
+
+            }
+        }
+    }
+
+
+    [ClientRpc]
+    void RpcTeleport() {
+        Instantiate(teleportParticles, transform.position, Quaternion.identity);
+        point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = point;
+        timeSinceTeleport = Time.time + teleportCooldown; // start the cooldown period
+    }
+
+    [Command]
+    void CmdTeleport() {
+        RpcTeleport();
+    }
+
+
+    void MovePlayer(Transform playerTransform) {
+        point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        playerTransform.transform.position = point;
+        timeSinceTeleport = Time.time + teleportCooldown; // start the cooldown period
+    }
+}
