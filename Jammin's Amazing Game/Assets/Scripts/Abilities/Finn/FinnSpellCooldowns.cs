77ed8530dc8e575ed4e6e5d7117685bb537461b4
Joel-Madsen <43053989+Joel-Madsen@users.noreply.github.com>
2018-11-01 20:30:45 -0600

    Added in Individual Character Spell UIs
    Each character now spawns with their own canvas related to their own unique abilities!

---
+++ Jammin's Amazing Game/Assets/Scripts/Abilities/Finn/FinnSpellCooldowns.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+public class FinnSpellCooldowns : NetworkBehaviour
+{
+
+    //public GameObject playerController;
+
+    public Image teleportCooldownImage; // The mask used for the radial cooldown UI
+    float teleportCooldown;
+    bool teleportCooldownActive;
+    bool blocked = false; // Was the teleport blocked by a wall bein in the way? (NOT USED, IN PROGRESS!)
+    Teleport teleportScript;
+
+    public Image puddleCooldownImage;
+    float puddleCooldown;
+    // Puddle puddleScript;  // UN-COMMENT WHEN THIS EXISTS
+    bool puddleCooldownActive;
+
+    void Start()
+    {
+
+
+        teleportCooldown = 3;//teleportScript.teleportCooldown;
+        puddleCooldown = 3;//puddleScript.cooldown; // get the cooldown for Spike's RockThrow Ability
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Code for Teleport (currently mapped to RMB)
+        if (!blocked)
+        {
+            if (Input.GetMouseButtonDown(1)) // if the player right clicks to teleport, activate the teleportCooldown UI
+            {
+                teleportCooldownActive = true;
+            }
+
+            if (teleportCooldownActive)
+            {
+                teleportCooldownImage.fillAmount += 1 / teleportCooldown * Time.deltaTime;
+
+                if (teleportCooldownImage.fillAmount >= 1) // after the radial animation plays, set everything back to its default state
+                {
+                    teleportCooldownImage.fillAmount = 0;
+                    teleportCooldownActive = false;
+                }
+            }
+        }
+
+
+        // 
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            puddleCooldownActive = true;
+        }
+
+        if (puddleCooldownActive)
+        {
+            puddleCooldownImage.fillAmount += 1 / puddleCooldown * Time.deltaTime;
+
+            if (puddleCooldownImage.fillAmount >= 1)
+            {
+                puddleCooldownImage.fillAmount = 0;
+                puddleCooldownActive = false;
+            }
+        }
+
+    }
+
+    public void TeleportBlocked(bool isBlocked)
+    {
+        blocked = isBlocked;
+    }
+
+
+
+}
