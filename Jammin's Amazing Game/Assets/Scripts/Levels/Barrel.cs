b4046ee081947a005dda30aff36f8782cd811dee
Nick <43053345+nkind@users.noreply.github.com>
2018-11-15 14:42:24 -0600

    Third room and basic barrel fireball interaction
    Added more borders to the rooms, it looks pretty rough so I'll have to work on that.

---
+++ Jammin's Amazing Game/Assets/Scripts/Levels/Barrel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Barrel : NetworkBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Fireball")
+        {
+            
+            Destroy(this.gameObject);
+        }
+    }
+
+}
--- Jammin's Amazing Game/Assets/Scripts/Levels/WinKey.cs
+++ Jammin's Amazing Game/Assets/Scripts/Levels/WinKey.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class WinKey : NetworkBehaviour {
-	
+
+    [SerializeField]
+    private int winKeyID; //use ID for distinguishing which room to jump to next
+
     /*
      * Center the camera into the first room once the game has started.  
      */
@@ -18,21 +21,51 @@ public class WinKey : NetworkBehaviour {
      */
     private void OnCollisionEnter2D(Collision2D collision) {
         // Center the camera into the second room when player touches key.
-        GameObject.Find("Main Camera").GetComponent<Camera>().transform.position = GameObject.Find("Room2 Center").transform.position;
-        SpawnPoints secondSpawn = GameObject.Find("Spawn2").GetComponent<SpawnPoints>();
+        if (winKeyID == 1)
+        {
+            GameObject.Find("Main Camera").GetComponent<Camera>().transform.position = GameObject.Find("Room2 Center").transform.position;
+            SpawnPoints secondSpawn = GameObject.Find("Spawn2").GetComponent<SpawnPoints>();
 
-        // Teleport the player that touched special key into the next room.
-        if (collision.gameObject.tag == "Player1") {
-            collision.gameObject.transform.position = secondSpawn.spawnPoints[0].transform.position;
+            // Teleport the player that touched special key into the next room.
+            if (collision.gameObject.tag == "Player1")
+            {
+                collision.gameObject.transform.position = secondSpawn.spawnPoints[0].transform.position;
+            }
+            if (collision.gameObject.tag == "Player2")
+            {
+                collision.gameObject.transform.position = secondSpawn.spawnPoints[1].transform.position;
+            }
+            if (collision.gameObject.tag == "Player3")
+            {
+                collision.gameObject.transform.position = secondSpawn.spawnPoints[2].transform.position;
+            }
+            if (collision.gameObject.tag == "Player4")
+            {
+                collision.gameObject.transform.position = secondSpawn.spawnPoints[3].transform.position;
+            }
         }
-        if (collision.gameObject.tag == "Player2") {
-            collision.gameObject.transform.position = secondSpawn.spawnPoints[1].transform.position;
-        }
-        if (collision.gameObject.tag == "Player3") {
-            collision.gameObject.transform.position = secondSpawn.spawnPoints[2].transform.position;
-        }
-        if (collision.gameObject.tag == "Player4") {
-            collision.gameObject.transform.position = secondSpawn.spawnPoints[3].transform.position;
+
+        if (winKeyID == 2)
+        {
+            GameObject.Find("Main Camera").GetComponent<Camera>().transform.position = GameObject.Find("Room3 Center").transform.position;
+            SpawnPoints thirdSpawn = GameObject.Find("Spawn3").GetComponent<SpawnPoints>();
+
+            if (collision.gameObject.tag == "Player1")
+            {
+                collision.gameObject.transform.position = thirdSpawn.spawnPoints[0].transform.position;
+            }
+            if (collision.gameObject.tag == "Player2")
+            {
+                collision.gameObject.transform.position = thirdSpawn.spawnPoints[1].transform.position;
+            }
+            if (collision.gameObject.tag == "Player3")
+            {
+                collision.gameObject.transform.position = thirdSpawn.spawnPoints[2].transform.position;
+            }
+            if (collision.gameObject.tag == "Player4")
+            {
+                collision.gameObject.transform.position = thirdSpawn.spawnPoints[3].transform.position;
+            }
         }
     }
 }
