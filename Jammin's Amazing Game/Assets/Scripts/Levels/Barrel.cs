9ef8ca5363c5062190efdc3ac5866accd59ceb9d
Nick <43053345+nkind@users.noreply.github.com>
2018-11-15 14:45:51 -0600

    Comments to barrel script

--- Jammin's Amazing Game/Assets/Scripts/Levels/Barrel.cs
+++ Jammin's Amazing Game/Assets/Scripts/Levels/Barrel.cs
@@ -7,7 +7,8 @@ public class Barrel : NetworkBehaviour {
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Fireball")
+        // Red Barrel can only be destroyed by Fireball for now
+        if (other.gameObject.tag == "Fireball") // looking to create a totem so Spike could also destroy it
         {
             
             Destroy(this.gameObject);
