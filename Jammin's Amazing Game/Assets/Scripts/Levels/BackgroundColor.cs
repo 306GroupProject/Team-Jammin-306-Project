b85ff7e932fd5c2f0f87ffcf0d2a914d73bd1023
Joel-Madsen <43053989+Joel-Madsen@users.noreply.github.com>
2018-12-05 15:59:06 -0600

    Added hearts to each normal room
    they heal the player to full on contact

---
+++ Jammin's Amazing Game/Assets/Scripts/Levels/BackgroundColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundColor : MonoBehaviour {
+
+    Color color1 = new Color(0.8207f, 0.2593f, 0.2708f, 1f);
+    Color color2 = new Color(0.9339f, 0.6308f, 0.1541f, 1f);
+    public Camera cam;
+
+    // Update is called once per frame
+    void Update () {
+        float time = Mathf.PingPong(Time.time, 4) / 4;
+        cam.backgroundColor = Color.Lerp(color1, color2, time);
+    }
+}
---
+++ Jammin's Amazing Game/Assets/Scripts/Levels/Heart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player1" || collision.tag == "Player2" || collision.tag == "Player3" || collision.tag == "Player4")
+        {
+            collision.GetComponent<playerHealth>().playerHP = 16;
+            Destroy(gameObject);
+        }
+    }
+}
